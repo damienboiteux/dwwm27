babc07b365e249205b5d6208e54600f5158c1985
damienboiteux.com <contact@damienboiteux.com>
2022-06-22 14:08:59 +0200

    mercredi 14h10

--- jquery.js
+++ jquery.js
@@ -3,3 +3,13 @@ document.querySelector(".para").addEventListener("click", function () {});
 
 // jQuery
 $(".para").on("click", function () {});
+
+// <a href="#" class="lien" mon-attribut="valeur">...
+
+let a = document.querySelector("a.lien");
+let valeur = a.attributes("mon-attribut").value();
+
+// <a href="#" class="lien" data-attribut="valeur">...
+// <a href="#" class="lien" data-src="./fff">...
+let valeur2 = a.data("attribut").value();
+let valeur3 = a.data("src").value();
